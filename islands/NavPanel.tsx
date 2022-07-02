efbee1e76c5c18a87f0ce87dab3026665e35f80c
LemuelKL <lemuellee.kl@gmail.com>
2022-07-02 19:58:09 +0800

    up

--- islands/NavPanel.tsx
+++ islands/NavPanel.tsx
@@ -14,51 +14,27 @@ export default function NavPanel(
     setActiveRoute: (route: Route) => void;
   },
 ) {
-  const [windowDimension, detectHW] = useState({
-    winWidth: window.innerWidth,
-    winHeight: window.innerHeight,
-  });
-
-  const detectSize = () => {
-    detectHW({
-      winWidth: window.innerWidth,
-      winHeight: window.innerHeight,
-    });
-  };
-
-  useEffect(() => {
-    self.addEventListener("resize", detectSize);
-    return () => {
-      self.removeEventListener("resize", detectSize);
-    };
-  }, [windowDimension]);
-
   return (
-    <div>
-      {windowDimension.winWidth >= 1280 &&
-        (
-          <div
-            class={tw
-              `bg-gray-800 h-screen max-w-sm flex-shrink flex flex-col gap-4 justify-center p-8`}
-          >
-            <div class={tw`flex-shrink`}>
-              <InfoCard></InfoCard>
-            </div>
-            <div
-              class={tw`flex-shrink flex flex-col gap-3 items-center`}
-            >
-              {routes.map((route) => {
-                return (
-                  <NavButton
-                    active={route.name === activeRoute.name}
-                    text={route.name}
-                    handleClick={() => setActiveRoute(route)}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
+    <div
+      class={tw
+        `bg-gray-800 h-screen max-w-sm flex-shrink flex flex-col gap-4 justify-center p-8`}
+    >
+      <div class={tw`flex-shrink`}>
+        <InfoCard></InfoCard>
+      </div>
+      <div
+        class={tw`flex-shrink flex flex-col gap-3 items-center`}
+      >
+        {routes.map((route) => {
+          return (
+            <NavButton
+              active={route.name === activeRoute.name}
+              text={route.name}
+              handleClick={() => setActiveRoute(route)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- islands/Router.tsx
+++ islands/Router.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 import Home from "../components/Home.tsx";
 import About from "../components/About.tsx";
@@ -31,14 +31,75 @@ export default function Router() {
     page: () => <div>Contact</div>,
   }];
   const [activeRoute, setActiveRoute] = useState(routes[1]);
+
+  const [windowDimension, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    self.addEventListener("resize", detectSize);
+    return () => {
+      self.removeEventListener("resize", detectSize);
+    };
+  }, [windowDimension]);
+
+  const [showNavPanel, setShowNavPanel] = useState(true);
   return (
     <div class={tw`w-screen h-screen flex justify-center bg-coolGray-600`}>
       <div class={tw`w-full max-w-7xl flex bg-green-500 shadow shadow-2xl`}>
-        <NavPanel
-          routes={routes}
-          activeRoute={activeRoute}
-          setActiveRoute={setActiveRoute}
-        />
+        {windowDimension.winWidth >= 1280 && showNavPanel &&
+          (
+            <NavPanel
+              routes={routes}
+              activeRoute={activeRoute}
+              setActiveRoute={setActiveRoute}
+            />
+          )}
+        <div
+          class={tw
+            `cursor-pointer bg-teal-900 hover:bg-teal-700 min-w-[0.5rem] ${
+              showNavPanel ? "max-w-[0.5rem]" : ""
+            } flex flex-col justify-center ${
+              showNavPanel ? "items-end" : "items-start"
+            }`}
+          onClick={() => setShowNavPanel(!showNavPanel)}
+          style={{ pointerEvents: showNavPanel ? "none" : "auto" }}
+        >
+          <div
+            class={tw
+              `cursor-pointer min-w-fit flex justify-center items-center rounded-tl-full rounded-bl-full p-1 hover:text-white ${
+                showNavPanel
+                  ? "bg-teal-900 hover:bg-teal-700 w-4 h-4 hover:w-6 hover:h-6"
+                  : "w-6 h-6"
+              }`}
+            style={{ pointerEvents: "auto" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class={tw`w-full`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={showNavPanel
+                  ? "M10 19l-7-7m0 0l7-7m-7 7h18"
+                  : "M14 5l7 7m0 0l-7 7m7-7H3"}
+              />
+            </svg>
+          </div>
+        </div>
         <div
           class={tw
             `bg-gray-900 text-white flex-grow flex justify-center items-center p-6`}
